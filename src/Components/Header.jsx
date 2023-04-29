2bfbacafbfd57cccbd72fcffc305e34ceb2451c2
Benjamin <goosta19802@gmail.com>
2023-04-29 07:55:56 -0300

    nuevos cambios

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import Img512 from '../Img/logo512.png'
+import '../Styles/header.css'
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <>
+            <div>Header</div>
+
+            <img
+                src={Img512}
+                className='img512-rojo'
+            >
+            </img>
+
+            <img
+                src={Img512}
+                className='img512-negro'
+            ></img>
+            <img
+                src={Img512}
+                className='img512-verde'
+            ></img>
+
+            <img
+                src={Img512}
+                className='img512-azul'
+            ></img>
+        </>
+    )
 }
 
 export default Header
