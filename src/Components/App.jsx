a56ccfe40e26689d9b973a2d66471061de5d27c9
Benjamin <goosta19802@gmail.com>
2023-05-02 10:58:02 -0300

    se agrego use state al app

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,61 @@
 import Header from './Header';
 import Footer from './Footer';
 import ContenidoPrincipal from './ContenidoPrincipal';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [nombre, setNombre] = useState("")
+
+
+  const [incrementar, setIncrementar] = useState(0)
+
+  const handleClickIncrementar = () => {
+    setIncrementar(incrementar + 1)
+  }
+
+  const handleClickDecrementar = () => {
+    if (incrementar > 0)
+      setIncrementar(incrementar - 1)
+  }
+
+
+  const handleClickReiniciar = () => {
+    if (incrementar > 0)
+      setIncrementar(0)
+  }
+
+
+
   return (
     <div>
-        <Header></Header>
-        <ContenidoPrincipal></ContenidoPrincipal>
-        <Footer></Footer>
+      {
+        incrementar >= 5 ?
+          <Header></Header>
+        : <h1>todavia no esta el header</h1>
+      }
+
+      <button
+        onClick={handleClickIncrementar}
+
+      >Incrementar</button>
+      <button
+        onClick={handleClickDecrementar}
+
+      >Decrementar</button>
+      <button
+        onClick={handleClickReiniciar}
+
+      >Reiniciar</button>
+
+      <h1>
+        {incrementar}
+      </h1>
+
+
+      <ContenidoPrincipal></ContenidoPrincipal>
+      <Footer></Footer>
     </div>
   );
 }
