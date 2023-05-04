42b9c2d96b09d6ae9a4438c4c8b47b4e787fd114
Benjamin <goosta19802@gmail.com>
2023-05-04 08:51:09 -0300

    aa

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,56 +3,36 @@ import Footer from './Footer';
 import ContenidoPrincipal from './ContenidoPrincipal';
 import { useState } from 'react';
 
-
 function App() {
+  const [nombre, setNombre] = useState('');
 
-  const [nombre, setNombre] = useState("")
-
-
-  const [incrementar, setIncrementar] = useState(0)
+  const [incrementar, setIncrementar] = useState(0);
 
   const handleClickIncrementar = () => {
-    setIncrementar(incrementar + 1)
-  }
+    setIncrementar(incrementar + 1);
+  };
 
   const handleClickDecrementar = () => {
-    if (incrementar > 0)
-      setIncrementar(incrementar - 1)
-  }
-
+    if (incrementar > 0) setIncrementar(incrementar - 1);
+  };
 
   const handleClickReiniciar = () => {
-    if (incrementar > 0)
-      setIncrementar(0)
-  }
-
-
+    if (incrementar > 0) setIncrementar(0);
+  };
 
   return (
     <div>
-      {
-        incrementar >= 5 ?
-          <Header></Header>
-        : <h1>todavia no esta el header</h1>
-      }
+      {incrementar >= 5 ? (
+        <Header></Header>
+      ) : (
+        <h1>todavia no esta el header</h1>
+      )}
 
-      <button
-        onClick={handleClickIncrementar}
+      <button onClick={handleClickIncrementar}>Incrementar</button>
+      <button onClick={handleClickDecrementar}>Decrementar</button>
+      <button onClick={handleClickReiniciar}>Reiniciar</button>
 
-      >Incrementar</button>
-      <button
-        onClick={handleClickDecrementar}
-
-      >Decrementar</button>
-      <button
-        onClick={handleClickReiniciar}
-
-      >Reiniciar</button>
-
-      <h1>
-        {incrementar}
-      </h1>
-
+      <h1>{incrementar}</h1>
 
       <ContenidoPrincipal></ContenidoPrincipal>
       <Footer></Footer>
