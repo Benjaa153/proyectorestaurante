1b4c54934b0b54476067c55696201b15518bb926
Benjamin <goosta19802@gmail.com>
2023-05-06 08:51:54 -0300

    cambios dia sabado

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,42 +1,23 @@
-import Header from './Header';
-import Footer from './Footer';
-import ContenidoPrincipal from './ContenidoPrincipal';
-import { useState } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Saludar from './Saludar';
+import ContenidoPrincipal from './ContenidoPrincipal'
+import ImgLogo from '../Img/logo192.png'
 function App() {
-  const [nombre, setNombre] = useState('');
 
-  const [incrementar, setIncrementar] = useState(0);
-
-  const handleClickIncrementar = () => {
-    setIncrementar(incrementar + 1);
-  };
-
-  const handleClickDecrementar = () => {
-    if (incrementar > 0) setIncrementar(incrementar - 1);
-  };
-  
-  const handleClickReiniciar = () => {
-    if (incrementar > 0) setIncrementar(0);
-  };
-
-    const nombreape = "BENJA"
+  const datosAlumno = {
+    nombre: 'Maximiliano',
+    apellido: 'Otta',
+    msjpara: "Hola",
+    edad: 30,
+    img: ImgLogo,
+    confirmado: true
+  }
   return (
     <div>
-      {incrementar >= 5 ? (
-        <Header></Header>
-      ) : (
-        <h1>todavia no esta el header</h1>
-      )}
 
-      <button onClick={handleClickIncrementar}>Incrementar</button>
-      <button onClick={handleClickDecrementar}>Decrementar</button>
-      <button onClick={handleClickReiniciar}>Reiniciar</button>
-
-      <h1>{incrementar}</h1>
-
-      <ContenidoPrincipal></ContenidoPrincipal>
-      <Footer nombreape={nombreape} ></Footer>
+      <Saludar
+          datosAlumno={datosAlumno}
+      />
     </div>
   );
 }
--- src/Components/ContenidoPrincipal.jsx
+++ src/Components/ContenidoPrincipal.jsx
@@ -8,17 +8,13 @@ const ContenidoPrincipal = () => {
       <h1 className="tituloSecundario">
         Contenido Principal
       </h1>
-      <div>
         <img className='imgLogo-verde' src={ImgLogo}></img>
-        <h1>Imagen logo en color verde</h1>
-      </div>
 
-      <div>
         <img className='imgLogo-amarillo' src={ImgLogo}></img>
-        <h1>Imagen logo en color amarillo</h1>
-      </div>
 
       <img className='imgLogo-naranja' src={ImgLogo}></img>
+
+
       <img className='imgLogo-aqua' src={ImgLogo}></img>
 
     </div>
